e5dcb1741e775bdff6f7973cb4549aab263856c1
talgat <Coderanonymouss>
2024-02-12 13:21:49 +0600

    COS-1

--- src/main/java/kz/satbayev/onlinestore/model/entity/ShopItems.java
+++ src/main/java/kz/satbayev/onlinestore/model/entity/ShopItems.java
@@ -1,4 +1,24 @@
 package kz.satbayev.onlinestore.model.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_items")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShopItems {
+
+    @Id
+    private Long id;
+    private String name;
+    private Long price;
+
 }
--- src/main/java/kz/satbayev/onlinestore/service/ItemService.java
+++ src/main/java/kz/satbayev/onlinestore/service/ItemService.java
@@ -1,4 +1,23 @@
 package kz.satbayev.onlinestore.service;
 
-public interface ItemService {
+import kz.satbayev.onlinestore.model.entity.ShopItems;
+import kz.satbayev.onlinestore.model.entity.ShopItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    private final ShopItemsRepository shopItemsRepository;
+
+    @Autowired
+    public ItemService(ShopItemsRepository shopItemsRepository) {
+        this.shopItemsRepository = shopItemsRepository;
+    }
+
+    public List<ShopItems> getAllItems() {
+        return shopItemsRepository.findAll();
+    }
 }
